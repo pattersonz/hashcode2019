e9337f84347444807e57b047856f6573e08e7f1c
Zach Patterson <zjp170130@cs89302.utdallas.edu>
2020-02-10 17:26:57 -0600

    primed input and output

--- solution.cpp
+++ solution.cpp
@@ -29,5 +29,49 @@ void greedy(vector<slide>& S)
 
 int main()
 {
+  vector<slide> slides, vs;
+  slides = vector<slide>(); vs = vector<slide>();
+  string file;
+  cout << "file: ";
+  cin >> file;
+  ifstream f;
+  f.open(file + ".in");
+  int N;
+  int M;
+  f >> N;
+  for (int i = 0; i < N; ++i)
+    {
+      char x;
+      f >> x;
+      slide y;
+      y.a = i;
+      y.b = -1;
+      y.tags = vector<string>();
+      int c;
+      f >> c;
+      for (int j = 0; j < c; ++j)
+	{
+	  string str;
+	  f >> str;
+	  y.tags.push_back(str);
+	}
+      if (x == 'V')
+	  vs.push_back(y);
+      else
+	slides.push_back(y);
+    }
+  f.close();
+  ofstream o;
+  o = ofstream(file + ".out", ofstream::out);
+  combineV(vs);
+  for (int i = 0; i < vs.size(); ++i)
+    slides.push_back(vs[i]);
+  greedy(slides);
+  o << slides.size() << endl;
+  for (int i = 0; i < slides.size(); ++i)
+    if (slides[i].b == -1)
+      o << slides[i].a << endl;
+    else
+      o << slides[i].a << " " << slides[i].b << endl;
   return 0;
 }
