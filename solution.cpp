1e1dfed9f9b90ce056347f043de51b5b5dbb65ad
Zach Patterson <zjp170130@cs89302.utdallas.edu>
2020-02-10 16:54:03 -0600

    skeleton

---
+++ solution.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class slide{
+public:
+  slide()
+  {
+    a = b = 0;
+    tags.clear();
+  }
+  int a,b;
+  vector<string> tags;
+};
+
+int score(struct slide a, struct slide b)
+{
+  return 0;
+}
+
+void combineV(vector<slide>& S)
+{
+  return;
+}
+
+void greedy(vector<slide>& S)
+{
+  return;
+}
+
+int main()
+{
+  return 0;
+}
