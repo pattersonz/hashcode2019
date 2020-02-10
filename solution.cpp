206f7e75648eb539b4c8464e395ae23c3be6ab5a
Zach Patterson <zjp170130@cs89302.utdallas.edu>
2020-02-10 17:52:58 -0600

    added combineV

--- solution.cpp
+++ solution.cpp
@@ -17,9 +17,34 @@ int score(struct slide a, struct slide b)
   return 0;
 }
 
+bool tagLenComp(slide a, slide b)
+{
+  return a.tags.size() > b.tags.size();
+}
+
 void combineV(vector<slide>& S)
 {
-  return;
+  sort(S.begin(), S.end(), tagLenComp);
+  int size = S.size() / 2;
+  for (int i = 0; i < size; ++i)
+    {
+      slide x = S.back();
+      S.pop_back();
+      S[i].b = x.a;
+      for (int j = 0; j < x.tags.size(); ++j)
+	{
+	  bool in = false;
+	  int k;
+	  for (k = 0; k < S[i].tags.size(); ++k)
+	    if (S[i].tags[k] == x.tags[j])
+	      {
+		in = true;
+		break;
+	      }
+	  if (in)
+	    S[i].tags.push_back(x.tags[k]);
+	}
+    }
 }
 
 void greedy(vector<slide>& S)
@@ -69,9 +94,9 @@ int main()
   greedy(slides);
   o << slides.size() << endl;
   for (int i = 0; i < slides.size(); ++i)
-    if (slides[i].b == -1)
-      o << slides[i].a << endl;
-    else
-      o << slides[i].a << " " << slides[i].b << endl;
+      if (slides[i].b == -1)
+	o << slides[i].a << endl;
+      else
+	o << slides[i].a << " " << slides[i].b << endl;
   return 0;
 }
